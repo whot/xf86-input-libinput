e9a0ee69cb81dea2280c0ae2eeea371c70d7911c
Peter Hutterer <peter.hutterer@who-t.net>
2015-03-02 10:42:03 +1000

    Don't unref the device until we're done with it in DEVICE_INIT
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/libinput.c
+++ src/libinput.c
@@ -632,12 +632,12 @@ xf86libinput_init(DeviceIntPtr dev)
 	if (libinput_device_has_capability(device, LIBINPUT_DEVICE_CAP_TOUCH))
 		xf86libinput_init_touch(pInfo);
 
+	LibinputInitProperty(dev);
+	XIRegisterPropertyHandler(dev, LibinputSetProperty, NULL, NULL);
+
 	/* unref the device now, because we'll get a new ref during
 	   DEVICE_ON */
 	libinput_device_unref(device);
-
-        LibinputInitProperty(dev);
-        XIRegisterPropertyHandler(dev, LibinputSetProperty, NULL, NULL);
 
 	return 0;
 }
