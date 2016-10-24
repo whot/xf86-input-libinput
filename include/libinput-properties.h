54cad3396489ae493cfeb0cfc8561e0252e1ada7
Peter Hutterer <peter.hutterer@who-t.net>
2016-10-24 14:41:51 +1000

    Implement stylus pressure curve support
    Takes a 4-point cubic bezier curve as input and maps the pressure coordinates
    to the values outlined by this curve. This is an extension of the current
    implementation in the xf86-input-wacom driver which only allows the two center
    control points to be modified.
    
    Over the years a few users have noted that the wacom driver's pressure curve
    makes it impossible to cap the pressure at a given value. Given our bezier
    implementation here, it's effectively a freebie to add configurability of the
    first and last control points. We do require all control points' x coordinates
    to be in ascending order.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- include/libinput-properties.h
+++ include/libinput-properties.h
@@ -183,4 +183,11 @@
 /* Device rotation: FLOAT, 1 value, 32 bit, read-only */
 #define LIBINPUT_PROP_ROTATION_ANGLE_DEFAULT "libinput Rotation Angle Default"
 
+/* Tablet tool pressure curve: float, 8 values, 32 bit
+ * Value range is [0.0, 1.0], the values specify the x/y of the four
+ * control points for a cubic bezier curve.
+ * Default value: 0.0 0.0 0.0 0.0 1.0 1.0 1.0 1.0
+ */
+#define LIBINPUT_PROP_TABLET_TOOL_PRESSURECURVE "libinput Tablet Tool Pressurecurve"
+
 #endif /* _LIBINPUT_PROPERTIES_H_ */
