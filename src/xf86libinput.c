0c657e0dcff4cff06a0d4cbea7dfac2a1d505cc3
Peter Hutterer <peter.hutterer@who-t.net>
2017-05-18 14:02:52 +1000

    Update copyright years
    because why not
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/xf86libinput.c
+++ src/xf86libinput.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2013-2015 Red Hat, Inc.
+ * Copyright © 2013-2017 Red Hat, Inc.
  *
  * Permission to use, copy, modify, distribute, and sell this software
  * and its documentation for any purpose is hereby granted without
